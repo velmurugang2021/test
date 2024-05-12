c6a4af17e5ce5ef979e6eba892d5e8f40ae80104
Velmurugan <velmurugan.govindas@gmail.com>
2024-05-13 03:12:19 +0530

    sample

--- src/main.tf
+++ src/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 resource "aws_instance" "EC2" {
   ami            = "ami-0f58b397bc5c1f2e8"
   instance_type  = "t.micro"
